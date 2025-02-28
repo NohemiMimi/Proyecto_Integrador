6d1c2efbb2eed32710993422cd3036a93ef8a244
Sergioloeraco <sergio.loeraco@gmail.com>
2025-02-27 21:26:59 -0600

    Update login-routing.modules.ts

--- src/app/pages/login/login-routing.modules.ts
+++ src/app/pages/login/login-routing.modules.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPage } from './login.page';
 
 const routes: Routes = [{ path: '', component: LoginPage }];
 
 export const LoginPageRoutingModule = RouterModule.forChild(routes);
-=======
-import { Routes, RouterModule } from '@angular/router';
-import { LoginPage } from './login.page';
 
-const routes: Routes = [{ path: '', component: LoginPage }];
-
-export const LoginPageRoutingModule = RouterModule.forChild(routes);
->>>>>>> f68db5b5a67a83e86346c2a24bba0cc9ed8f5fe2
