fc43a6049db5abaf9fa2630cf889d1feb7a77902
Sergioloeraco <sergio.loeraco@gmail.com>
2025-02-27 21:25:40 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -16,22 +15,4 @@ bootstrapApplication(AppComponent, {
     importProvidersFrom(ReactiveFormsModule), 
   ],
 });
-=======
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { importProvidersFrom } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-    importProvidersFrom(ReactiveFormsModule), 
-  ],
-});
->>>>>>> f68db5b5a67a83e86346c2a24bba0cc9ed8f5fe2
