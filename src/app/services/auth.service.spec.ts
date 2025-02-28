11a22f37dc4163a7bb0f52a760aa4e056aed4c8c
Sergioloeraco <sergio.loeraco@gmail.com>
2025-02-27 21:29:17 -0600

    Update auth.service.spec.ts

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
@@ -15,21 +14,4 @@ describe('AuthService', () => {
     expect(service).toBeTruthy();
   });
 });
-=======
-import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
-
-describe('AuthService', () => {
-  let service: AuthService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
->>>>>>> f68db5b5a67a83e86346c2a24bba0cc9ed8f5fe2
