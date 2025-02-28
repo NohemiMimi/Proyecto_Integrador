55501b47cd51075c2ca230752a78a2dcc3393e27
Sergioloeraco <sergio.loeraco@gmail.com>
2025-02-27 21:28:10 -0600

    Update login.page.spec.ts

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPage } from './login.page';
 
@@ -16,22 +15,4 @@ describe('LoginPage', () => {
     expect(component).toBeTruthy();
   });
 });
-=======
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { LoginPage } from './login.page';
 
-describe('LoginPage', () => {
-  let component: LoginPage;
-  let fixture: ComponentFixture<LoginPage>;
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
->>>>>>> f68db5b5a67a83e86346c2a24bba0cc9ed8f5fe2
