92f712ebd85f6dde09c35b8dfe8f0d9aa4702c8b
Sergioloeraco <sergio.loeraco@gmail.com>
2025-02-27 21:03:10 -0600

    Delete pages/login directory

--- pages/login/login-routing.modules.ts
+++
@@ -1,6 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { LoginPage } from './login.page';
-
-const routes: Routes = [{ path: '', component: LoginPage }];
-
-export const LoginPageRoutingModule = RouterModule.forChild(routes);
